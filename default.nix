8842408b8369b61110ab819b20fcee1e6a8b29d4
Cmdv <vincent@cmdv.me>
2020-10-11 22:49:02 +0100

    add haskell-8-10-2

--- default.nix
+++ default.nix
@@ -3,10 +3,11 @@ let
   pkgs = import sources.nixpkgs {};
   easy-ps = import sources.easy-purescript-nix { inherit pkgs; };
   haskell = import ./haskell.nix { inherit pkgs; };
+  haskell-8-10-2 = import ./haskell-8-10-2.nix { inherit pkgs; };
   purescript = import ./purescript.nix { inherit pkgs easy-ps; };
 
   self = {
-    inherit pkgs haskell purescript;
+    inherit pkgs haskell haskell-8-10-2 purescript;
   };
 
 in
---
+++ haskell-8-10-2.nix
@@ -0,0 +1,26 @@
+{ pkgs }:
+let
+  ghc = pkgs.haskell.packages.ghc8102;
+
+
+  self = rec {
+    inherit ghc;
+    cabal-install = ghc.cabal-install;
+    stylish-haskell = ghc.stylish-haskell;
+    haskell-language-server = ghc.haskell-language-server;
+    hlint = ghc.hlint;
+    ghc883 = pkgs.haskell.compiler.ghc8102;
+
+    mkShell = pkgs.mkShell {
+      buildInputs = [
+        self.ghc8102
+        self.stylish-haskell
+        self.hlint
+        self.cabal-install
+        self.haskell-language-server
+      ];
+    };
+  };
+
+in
+  self
